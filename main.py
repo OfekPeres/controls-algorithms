fixed error with missing sign on calcThetaError

main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from src.dynamics import dubins_car
 from src.dynamics.dubins_car import DubinsCar
 from src.controls.bang_bang import BangBang
 def testBangBang():
@@ -16,5 +17,24 @@ def testBangBang():
     # print(traj)
     print("Final car position: {}".format(car.pos))
 
+
+
+def testStep():
+    car = DubinsCar(10, 0, 0, 0, 0, 1)
+    # car.step(0.5)
+    car.step(0.5)
+    for i in range(10):
+        car.step(0)
+    print(car.pos)
+
+
+def testThetaError():
+    theta = 0
+    theta = np.deg2rad(theta)
+    car = DubinsCar(10,0,0,theta,0,1)
+    thetaErrorRadians = car.calcThetaError([1,-1])
+    print(np.rad2deg(thetaErrorRadians))
 if __name__ == "__main__":
-    testBangBang()
+    # testBangBang()
+    # testStep()
+    testThetaError()

src/dynamics/dubins_car.py
@@ -34,8 +34,10 @@ class DubinsCar:
         """
         carHeading = np.array([np.cos(self.theta), np.sin(self.theta)])
         goalPoint = goalPoint / np.linalg.norm(goalPoint)
-        thetaError = np.arccos(np.dot(carHeading,goalPoint))
-        return thetaError
+        # thetaError = np.arccos(np.dot(carHeading,goalPoint))
+        angle = math.atan2( carHeading[0]*goalPoint[1] - carHeading[1]*goalPoint[0], 
+                            carHeading[0]*goalPoint[0]+carHeading[1]*goalPoint[1])
+        return angle
 
 
 if __name__ == "__main__":