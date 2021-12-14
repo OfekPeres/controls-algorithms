08b49f26bddbb0c72f2cb6ea2ec772629e29de54
Ofek Peres <operes@princeton.edu>
2021-12-14 11:14:10 -0500

    initial gh-pages commit

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-from .dynamics.dubins_car import DubinsCar
-
-
-
-if __name__ == "__main__":
-    print('main module in init')
--- src/controls/converter.py
+++
@@ -1,2 +0,0 @@
-def GetWaypointsFromDubinsPathOuput(DubinsPathOutput):
-    return DubinsPathOutput["waypoints"]
